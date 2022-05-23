4937995ce8870b0a6ecc93f1e61d67dc8f6b5d9e
zeeker <zeekerpro@gmail.com>
2022-05-24 01:27:25 +0800

    feat: dynamic load micro apps

--- base/src/App.vue
+++ base/src/App.vue
@@ -3,20 +3,45 @@
 
     <h1 style="color: blue;" > I am apps base container </h1>
 
-    <sub-app
-      name="app1"
-      url="http://localhost:3101/"
-    />
+    <template
+      v-for="app in apps"
+      :key="app.id"
+      >
 
-    <sub-app
-      name="app2"
-      url="http://localhost:3102/"
-    />
+      <sub-app
+        :name="app.name"
+        :url="app.url"
+      />
 
+    </template>
 
   </div>
 </template>
 
 <script lang="ts" setup>
-import microApp, { getActiveApps } from '@micro-zoe/micro-app'
+import { ref, onMounted } from "vue";
+
+interface AppModelType {
+  id: number,
+  name: string,
+  url: string,
+  base: string
+}
+
+const apps = ref<Array<AppModelType>>([]);
+
+async function getApps(){
+  const ret = await fetch("http://localhost:4000/apps", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  apps.value = await ret.json() as Array<AppModelType>;
+}
+
+onMounted(() => {
+  getApps();
+})
+
 </script>
--- base/src/vendors/micro/index.ts
+++ base/src/vendors/micro/index.ts
@@ -1,38 +1,63 @@
 import microApp from "@micro-zoe/micro-app";
 
-const microConfigs = {
-	plugins: {
-		modules: {
-			'app1': [
-				{
-					loader(code){
-						if (process.env.NODE_ENV === 'development') {
-							// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-							code = code.replace(/(from|import)(\s*['"])(\/sites\/app1\/)/g, all => {
-								return all.replace('/sites/app1/', 'http://localhost:3101/')
-							})
-						}
-						return code;
-					}
-				}
-			],
-			'app2': [
-				{
-					loader(code){
-						if (process.env.NODE_ENV === 'development') {
-							// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-							code = code.replace(/(from|import)(\s*['"])(\/sites\/app2\/)/g, all => {
-								return all.replace('/sites/app2/', 'http://localhost:3102/')
-							})
-						}
-						return code;
-					}
-				}
-			]
-		}
-	}
+interface AppModelType {
+  id: number,
+  name: string,
+  url: string,
+	base: string
 }
 
+const generateConfig = async () => {
+  const ret = await fetch("http://localhost:4000/apps", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  const apps = await ret.json() as Array<AppModelType>;
+
+	// TODO: generage apps micro config
+
+	const microConfigs = {
+		plugins: {
+			modules: {
+				[apps[0].name]: [
+					{
+						loader(code: any){
+							if (process.env.NODE_ENV === 'development') {
+								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
+								const reg = new RegExp(`(from|import)(\s*['"])(\/sites\/app1\/)`, 'g');
+								code = code.replace(/(from|import)(\s*['"])(\/sites\/app1\/)/g, (all :any) => {
+									return all.replace('/sites/app1/', 'http://localhost:3101/sites/app1/')
+								})
+							}
+							return code;
+						}
+					}
+				],
+				'app2': [
+					{
+						loader(code: any){
+							if (process.env.NODE_ENV === 'development') {
+								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
+								code = code.replace(/(from|import)(\s*['"])(\/sites\/app2\/)/g, (all :any) => {
+									return all.replace('/sites/app2/', 'http://localhost:3102/sites/app2/')
+								})
+							}
+							return code;
+						}
+					}
+				]
+			}
+		}
+	}
+
+	return microConfigs;
+};
+
+
 export const setupMicro = () => {
-	microApp.start(microConfigs);
+	generateConfig().then((microConfig) => {
+		microApp.start(microConfig);
+	})
 }
