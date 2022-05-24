92e4295dbe2a8b97b77c62d90611bc3cb34cd27c
zeeker <zeekerpro@gmail.com>
2022-05-24 23:52:19 +0800

    feat: support dynamic registor apps

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	semi: false,
-	singleQuote: true,
-	printWidth: 90,
-	tabWidth: 2,
-}
--- base/src/vendors/micro/index.ts
+++ base/src/vendors/micro/index.ts
@@ -16,51 +16,33 @@ const generateConfig = async () => {
 	})
 
 	const apps = (await ret.json()) as Array<AppModelType>
-	// TODO: generage apps micro config
+
+	const modules = {};
+
+	apps.forEach(app => {
+		const loader = (code: any) => {
+			if (process.env.NODE_ENV === 'development') {
+				// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
+				let basename = app.base;
+				const reg = new RegExp(`(from|import)(\\s*['"])(${basename})`, 'g');
+				code = code.replace(
+					reg,
+					(all: any) => {
+						return all.replace(
+							basename,
+							`${new URL(basename, app.url)}`
+						)
+					}
+				)
+			}
+			return code;
+		};
+		Reflect.set(modules, app.name, [ { loader } ]);
+	})
 
 	const microConfigs = {
 		plugins: {
-			modules: {
-				[apps[0].name]: [
-					{
-						loader(code: any) {
-							if (process.env.NODE_ENV === 'development') {
-								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-								// const reg = new RegExp(`(from|import)(\s*['"])(\/sites\/app1\/)`, 'g');
-								code = code.replace(
-									/(from|import)(\s*['"])(\/sites\/app1\/)/g,
-									(all: any) => {
-										return all.replace(
-											'/sites/app1/',
-											'http://localhost:3101/sites/app1/'
-										)
-									}
-								)
-							}
-							return code
-						},
-					},
-				],
-				app2: [
-					{
-						loader(code: any) {
-							if (process.env.NODE_ENV === 'development') {
-								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-								code = code.replace(
-									/(from|import)(\s*['"])(\/sites\/app2\/)/g,
-									(all: any) => {
-										return all.replace(
-											'/sites/app2/',
-											'http://localhost:3102/sites/app2/'
-										)
-									}
-								)
-							}
-							return code
-						},
-					},
-				],
-			},
+			modules
 		},
 	}
 
