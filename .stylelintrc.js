4e321f2d8956bbde15e2ba88021c3df483c880f6
zeeker <zeekerpro@gmail.com>
2022-05-24 09:40:59 +0800

    build: list-staged

---
+++ .stylelintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-recommended-vue',
+		'stylelint-config-prettier',
+	],
+	customSyntax: 'postcss-scss',
+	plugins: ['stylelint-scss'],
+	overrides: [
+		{
+			files: ['*.vue', '**/*.vue'],
+			customSyntax: 'postcss-html',
+		},
+	],
+	rules: {
+		/** Font icons */
+		'font-family-no-missing-generic-family-keyword': null,
+
+		/** SCSS **/
+		'at-rule-no-unknown': null,
+		'no-descending-specificity': null,
+		'scss/at-mixin-pattern': null,
+		'keyframes-name-pattern': null,
+		'selector-class-pattern': null,
+		'custom-property-pattern': null,
+		'declaration-block-no-redundant-longhand-properties': null,
+
+		/** Vuejs **/
+		'value-keyword-case': null,
+		'custom-property-empty-line-before': null,
+		'selector-pseudo-element-no-unknown': [
+			true,
+			{
+				ignorePseudoElements: ['/^v-deep/'],
+			},
+		],
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: ['/^deep/', '/^slotted/', '/^global/'],
+			},
+		],
+		'value-keyword-case': [
+			'lower',
+			{
+				ignoreFunctions: ['v-bind'],
+			},
+		],
+	},
+}
--- base/src/vendors/micro/index.ts
+++ base/src/vendors/micro/index.ts
@@ -1,21 +1,21 @@
-import microApp from "@micro-zoe/micro-app";
+import microApp from '@micro-zoe/micro-app'
 
 interface AppModelType {
-  id: number,
-  name: string,
-  url: string,
+	id: number
+	name: string
+	url: string
 	base: string
 }
 
 const generateConfig = async () => {
-  const ret = await fetch("http://localhost:4000/apps", {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
-  const apps = await ret.json() as Array<AppModelType>;
+	const ret = await fetch('http://localhost:4000/apps', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
 
+	const apps = (await ret.json()) as Array<AppModelType>
 	// TODO: generage apps micro config
 
 	const microConfigs = {
@@ -23,41 +23,52 @@ const generateConfig = async () => {
 			modules: {
 				[apps[0].name]: [
 					{
-						loader(code: any){
+						loader(code: any) {
 							if (process.env.NODE_ENV === 'development') {
 								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-								const reg = new RegExp(`(from|import)(\s*['"])(\/sites\/app1\/)`, 'g');
-								code = code.replace(/(from|import)(\s*['"])(\/sites\/app1\/)/g, (all :any) => {
-									return all.replace('/sites/app1/', 'http://localhost:3101/sites/app1/')
-								})
+								// const reg = new RegExp(`(from|import)(\s*['"])(\/sites\/app1\/)`, 'g');
+								code = code.replace(
+									/(from|import)(\s*['"])(\/sites\/app1\/)/g,
+									(all: any) => {
+										return all.replace(
+											'/sites/app1/',
+											'http://localhost:3101/sites/app1/'
+										)
+									}
+								)
 							}
-							return code;
-						}
-					}
+							return code
+						},
+					},
 				],
-				'app2': [
+				app2: [
 					{
-						loader(code: any){
+						loader(code: any) {
 							if (process.env.NODE_ENV === 'development') {
 								// 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-								code = code.replace(/(from|import)(\s*['"])(\/sites\/app2\/)/g, (all :any) => {
-									return all.replace('/sites/app2/', 'http://localhost:3102/sites/app2/')
-								})
+								code = code.replace(
+									/(from|import)(\s*['"])(\/sites\/app2\/)/g,
+									(all: any) => {
+										return all.replace(
+											'/sites/app2/',
+											'http://localhost:3102/sites/app2/'
+										)
+									}
+								)
 							}
-							return code;
-						}
-					}
-				]
-			}
-		}
+							return code
+						},
+					},
+				],
+			},
+		},
 	}
 
-	return microConfigs;
-};
-
+	return microConfigs
+}
 
 export const setupMicro = () => {
 	generateConfig().then((microConfig) => {
-		microApp.start(microConfig);
+		microApp.start(microConfig)
 	})
 }
